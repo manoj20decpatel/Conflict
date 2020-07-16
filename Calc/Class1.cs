9eccb6405238eabeeca62eac0cab583c43ff95e2
Manoj <Manoj@crmuser.com>
2020-07-16 10:38:25 +0530

    All changes

---
+++ Calc/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Calc
+{
+    public class Class1
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Calc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, Class1.Sum(0, 1));
+        }
+    }
+}
